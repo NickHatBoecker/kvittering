f068187aa8cd98104df1e33c198805ee0e548035
NickHatBoecker <info@nick-hat-boecker.de>
2023-05-28 00:16:58 +0200

    Initial dist subtree commit

---
+++ precache-manifest.8c25b3284accc0c3926c839182a49076.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99dc0b91c6e34074485f",
+    "url": "/kvittering/css/app.8d9b60d9.css"
+  },
+  {
+    "revision": "e7f154f3ff981bc73aa1",
+    "url": "/kvittering/css/chunk-vendors.f5fd725b.css"
+  },
+  {
+    "revision": "db4ff89e0cd9acd378b34ccf06ffbdba",
+    "url": "/kvittering/img/page-pattern.db4ff89e.png"
+  },
+  {
+    "revision": "5ee1c9de0e92d5272503b478e1ec418f",
+    "url": "/kvittering/index.html"
+  },
+  {
+    "revision": "99dc0b91c6e34074485f",
+    "url": "/kvittering/js/app.05b4b986.js"
+  },
+  {
+    "revision": "e7f154f3ff981bc73aa1",
+    "url": "/kvittering/js/chunk-vendors.3e62d7ae.js"
+  },
+  {
+    "revision": "36a8b2568d70ad184eae",
+    "url": "/kvittering/js/sign-up.85fb1744.js"
+  },
+  {
+    "revision": "fca80971d5c5b558250e",
+    "url": "/kvittering/js/trash.c2e3a766.js"
+  },
+  {
+    "revision": "4be8c9c16f1a29dab3e8161f28e1fec6",
+    "url": "/kvittering/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kvittering/robots.txt"
+  }
+]);
